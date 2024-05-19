ea107a1c97dcfd6bd24ea215a8d1ce63a6c42ef5
Teja Peddi <tejakumarreddy852001@gmail.com>
2024-05-19 11:19:06 -0400

    Update webapp.py

--- app/webapp.py
+++ app/webapp.py
@@ -7,7 +7,7 @@ html, body, [class*="View"] {
     padding: 0px !important;
 }
 .stApp {
-    background-image: url("https://wallpapercave.com/wp/wp3982250.jpg");
+    background-image: url("https://github.com/tejapeddi1/UMBC-DATA606-Capstone/blob/main/app/bg_img.png?raw=true");
     background-size: cover;
     background-position: right;
     background-repeat: no-repeat;
